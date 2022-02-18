1e0255118a3cb3c3052924b8faf8ec9e555d0644
Ed Younskevicius <ed@codefellows.com>
2022-02-18 15:38:08 -0800

    Added skeleton code for linked list

--- class-05/demo/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ class-05/demo/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -2,9 +2,22 @@ package datastructures.linkedlist;
 
 public class LinkedList
 {
+  Node head = null;
+  Node tail = null;  // not truly required, but can be useful
+
+  public void insert(int value)
+  {
+    // TODO: implement me
+  }
+
+  public boolean includes(int value)
+  {
+    return false;  // TODO implement me
+  }
+
   @Override
   public String toString()
-    {
-        return "null";  // TODO: implement me
-    }
+  {
+    return "null";  // TODO: implement me
+  }
 }
