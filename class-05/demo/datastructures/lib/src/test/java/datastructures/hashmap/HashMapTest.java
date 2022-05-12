13a92b7481e711c61f466dabfef0d22a3ae3839a
Ed Younskevicius <ed@codefellows.com>
2022-05-12 14:54:19 -0700

    Add HashMapTest.java

---
+++ class-05/demo/datastructures/lib/src/test/java/datastructures/hashmap/HashMapTest.java
@@ -0,0 +1,12 @@
+package datastructures.hashmap;
+
+import org.junit.jupiter.api.Test;
+
+public class HashMapTest
+{
+  @Test
+  void testAMethod()
+  {
+    HashMap<String, String> sut = new HashMap<>(10);
+  }
+}
